262a0c31906483f481380a0a4e09a815b0a2810a
26gcode <166840469+26Gcode@users.noreply.github.com>
2025-01-11 11:03:04 -0600

    EvoSim 1.0 wip

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import numpy as np
+
+def main():
+    pygame.init
+    screen = pygame.display.set_caption('SandboxRealms Pre-Alpha 1.0.0')
+    game_window = pygame.display.set_mode((window_x, window_y))
+def update():
