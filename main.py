913f0ddbcab76c54a1615fb6922a24ec383fbab5
26gcode <166840469+26Gcode@users.noreply.github.com>
2025-01-25 11:02:04 -0600

    EVOsim wip

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pygame
 import numpy as np
+from pygame import Vector2
 
-def main():
-    pygame.init
-    screen = pygame.display.set_caption('SandboxRealms Pre-Alpha 1.0.0')
-    game_window = pygame.display.set_mode((window_x, window_y))
-def update():
+screen_size = Vector2(800,600)
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_caption('SandboxRealms Pre-Alpha 1.0.0')
+        self.screen = pygame.display.set_mode(screen_size, flags=pygame.SCALED)
+    
+    def run(self):
+        self.running = True
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type ==pygame.QUIT:
+                    self.running = False
+
+
+if __name__== '__main__':
+    Game().run()
