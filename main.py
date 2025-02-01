30adab53a996d3ee9aaf7383f7102a3b1388df05
26gcode <166840469+26Gcode@users.noreply.github.com>
2025-02-01 10:07:00 -0600

    fixed code

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import pygame
-import numpy as np
+import numpy
 from pygame import Vector2
+import random
 
 screen_size = Vector2(800,600)
 
-class Game:
+class Particle:
+    def __init__(self, size, color, position):
+        self.size = size
+        self.color = color
+        self.position = Vector2(position)
+        
+class ParticleManager:
+    def __init__(self):
+        self.particles = []
+
+    def update(self):
+        test = 1
+
+    def add(self, particles):
+        self.particles.extend(particles)
+
+
+    def draw(self):
+        test = 2
+
+
+class Sim:
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_caption('SandboxRealms Pre-Alpha 1.0.0')
+        self.clock = pygame.time.Clock() 
+
+        self.pm = ParticleManager()
+
+        self.screen = pygame.display.set_caption('EVOsim')
         self.screen = pygame.display.set_mode(screen_size, flags=pygame.SCALED)
     
+    def update(self):
+        self.pm.add([Particle((random.randinit(1,3), random.choice(['red', 'blue', 'green']), random.randit(0, screen_size.x), -10))])
+
+    def draw(self, surface):
+        surface.fill('black')
+        pygame.display.flip()
+
     def run(self):
         self.running = True
 
@@ -18,7 +51,11 @@ class Game:
             for event in pygame.event.get():
                 if event.type ==pygame.QUIT:
                     self.running = False
+            
+
+            self.draw(self.screen)
 
 
 if __name__== '__main__':
-    Game().run()
+    Sim().run()
+
