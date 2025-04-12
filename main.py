548dee91150b26fa71cc227becafa2140009b02f
Gavin <gavinwbush@gmail.com>
2025-04-12 09:53:52 -0600

    fixed eaating and multiplying, adding restrictions and plant multiplying

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from datetime import datetime
 import numpy as np
 
 PARTICLE_NUMBER = 200
@@ -27,32 +28,37 @@ RADII = np.array([
     [75, 75, 75],  
 ])
 
+last_eaten_timer = np.zeros(PARTICLE_NUMBER,dtype=int)
 eat_counts = np.zeros(PARTICLE_NUMBER,dtype=int)
 
-def update_particles(positions, velocity, types, eat_counts):
-    new_position = np.empty_like(positions)
+def update_particles(pos, velocity, types, eat_counts, last_eaten_timer):
+    num_particles = len(pos)
+    new_position = np.empty_like(pos)
     new_velocity = np.empty_like(velocity)
 
-    for i in range(PARTICLE_NUMBER):
+    for i in range(num_particles):
         total_force_x, total_force_y = 0.0, 0.0
-        pos_x, pos_y = positions[i]
+        pos_x, pos_y = pos[i]
         vel_x, vel_y = velocity[i]
         p_type = types[i]
 
         if p_type == 1:
             new_position[i] = pos_x, pos_y
             new_velocity[i] = 0, 0
+            last_eaten_timer[i] = 0
             continue
 
-        for j in range(len(pos)):
+        for j in range(num_particles):
             if i != j:
-                dir_x = positions[j, 0] - pos_x
-                dir_y = positions[j, 1] - pos_y
+                dir_x = pos[j, 0] - pos_x
+                dir_y = pos[j, 1] - pos_y
 
                 dis = np.sqrt(dir_x**2 + dir_y**2)
                 if dis > 0:
                     dir_x, dir_y = dir_x / dis, dir_y / dis 
                     other_type = types[j]
+
+                    #print(other_type)
 
                     if dis < MIN_DISTANCES[p_type, other_type]:
                         force = abs(FORCES[p_type, other_type]) * -3 * (1 - dis / MIN_DISTANCES[p_type, other_type]) * K
@@ -91,8 +97,8 @@ def main():
     pos = np.random.rand(PARTICLE_NUMBER, 2) * [WIDTH, HEIGHT]
     types = np.random.randint(0, NUM_TYPES, PARTICLE_NUMBER)
     velocity = np.zeros((PARTICLE_NUMBER, 2))
+    global last_eaten_timer
     global eat_counts
-    
     running = True
     while running:
         for event in pygame.event.get():
@@ -101,34 +107,35 @@ def main():
         
         screen.fill((0, 0, 0))
 
-        pos, velocity, types, eat_counts = update_particles(pos, velocity, types, eat_counts)
+        pos, velocity, types, eat_counts, last_eaten_timer = update_particles(pos, velocity, types, eat_counts, last_eaten_timer)
 
-        new_positions, new_velocities, new_types = [], [], []
+        new_pos, new_velocities, new_types = [], [], []
         for i in range(len(pos)):
             if types[i] != -1: 
-                new_positions.append(pos[i])
+                new_pos.append(pos[i])
                 new_velocities.append(velocity[i])
                 new_types.append(types[i])
             
                 if types[i] == 0 and eat_counts[i] >= RED_EAT_MINIMUM:
-                    new_positions.append(pos[i] + np.random.uniform(-5,5,size=2))
+                    new_pos.append(pos[i] + np.random.uniform(-5,5,size=2))
                     new_velocities.append([0,0])
                     new_types.append(0)
                     eat_counts[i] = 0
                 
                 if types[i] == 2 and eat_counts[i] >= BLUE_EAT_MINIMUM:
-                    new_positions.append(pos[i] + np.random.uniform(-5,5,size=2))
+                    new_pos.append(pos[i] + np.random.uniform(-5,5,size=2))
                     new_velocities.append([0,0])
                     new_types.append(2)
                     eat_counts[i] = 0
 
-        pos = np.array(new_positions)
+        pos = np.array(new_pos)
         velocity = np.array(new_velocities)
+        types = np.array(new_types)
         
-        for i in range(len(pos)):
+        for i in range(len(pos) - 1):
             color = [(255,0,0),(0,255,0),(0,0,255)][types[i]]
             #color.hsva = (types[i] * COLOR_STEP, 100,100,100)
-            print(i)
+            # print(i)
             pygame.draw.circle(screen, color, (int(pos[i, 0]), int(pos[i, 1])), 2)
         
         pygame.display.flip()
